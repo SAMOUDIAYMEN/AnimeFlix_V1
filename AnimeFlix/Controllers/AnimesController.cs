7e2d0f6a2cf5c1b1112db6b68a5a6cbc50c8e20c
Aymen SAMOUDI <samoudiayman777@outlook.com>
2021-05-19 23:10:52 +0100

    add all animes page

---
+++ AnimeFlix/Controllers/AnimesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Anime.Data;
+using AnimeFlix.Models;
+
+namespace AnimeFlix.Controllers
+{
+    public class AnimesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AnimesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Animes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Anime.ToListAsync());
+        }
+
+        // GET: Animes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var manageAnime = await _context.Anime
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (manageAnime == null)
+            {
+                return NotFound();
+            }
+
+            return View(manageAnime);
+        }
+
+    }
+}
