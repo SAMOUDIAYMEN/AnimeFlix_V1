9d946a9bd062083516f64ee14b6360d585193d79
Aymen SAMOUDI <samoudiayman777@outlook.com>
2021-05-19 15:58:23 +0100

    ready to migration

---
+++ AnimeFlix/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AnimeFlix.Models;
+
+namespace Anime.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<ManageAnime> Anime { get; set; }
+    }
+}
---
+++ AnimeFlix/Models/ManageAnime.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace AnimeFlix.Models
+{
+    public class ManageAnime
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        public string titleAnime { get; set; }
+        [Required]
+        public string src { get; set; }
+        [Required]
+        public string studion { get; set; }
+        [Required]
+        public string author { get; set; }
+        [Required]
+        public string descriptionAnime { get; set; }
+        [Required]
+        public int ratingAnime { get; set; }
+        [Required]
+        public string imgAnime { get; set; }
+        [Required]
+        public string trailerAnime { get; set; }
+        [Required]
+        public string runtime { get; set; }
+        [Required]
+        public int categoryAnime { get; set; }
+    }
+}
